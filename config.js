0e1491fa99a4cf4665adb5c5fb05ecf3b736b37a
clintandrewhall <clint@clintandrewhall.com>
2018-04-15 14:58:33 -0400

    Fixes to get it running again. Better code on the other branch

--- config.js
+++ config.js
@@ -1,70 +1,69 @@
-
-exports.config = function() {
-
+require('dotenv').config();
+exports.config = (function() {
   var appDefaults = {
-    "concurrentCalls" : 3,
-    "before" : 0,
-    "after" : 0,
-    "limit" : 250,
-    "homeRadius" : 50,
-    "cityRadius" : 20,
-    "distanceUnit" : "miles",
-    "tripMinimum" : 2
+    concurrentCalls: 3,
+    before: 0,
+    after: 0,
+    limit: 250,
+    homeRadius: 50,
+    cityRadius: 20,
+    distanceUnit: 'miles',
+    tripMinimum: 2,
   };
 
   var configs = {
-    "remote" : {
-      "port" : process.env.PORT || 3000,
-      "session" : {
-        "secret" : process.env.SESSION_SECRET,
-        "age" : 60000,
-        "impl" : "memory"
+    remote: {
+      port: process.env.PORT || 3000,
+      session: {
+        secret: process.env.SESSION_SECRET,
+        age: 60000,
+        impl: 'memory',
       },
-      "yahoo" : {
-        "appId" : process.env.YAHOO_APP_ID
+      zip: {
+        api: process.env.ZIP_API_ID,
       },
-      "foursquare" : {
-        "secrets" : {
-          "redirectUrl" : process.env.FSQ_REDIRECT,
-          "clientId" : process.env.FSQ_CLIENT_ID,
-          "clientSecret" : process.env.FSQ_CLIENT_SECRET,
+      foursquare: {
+        secrets: {
+          redirectUrl: process.env.FSQ_REDIRECT,
+          clientId: process.env.FSQ_CLIENT_ID,
+          clientSecret: process.env.FSQ_CLIENT_SECRET,
         },
-        "startYear" : 2009
+        startYear: 2009,
       },
-      "defaults" : appDefaults
+      defaults: appDefaults,
     },
-    "local" : {
-      "port" : 3000,
-      "session" : {
-        "secret" : "YOUR SECRET",
-        "age" : 3000,
-        "impl" : "memory"
+    local: {
+      port: 3000,
+      session: {
+        secret: process.env.SESSION_SECRET,
+        age: 3000,
+        impl: 'memory',
       },
-      "yahoo" : {
-        "appId" : "YOUR APP ID"
+      yahoo: {
+        appId: 'YOUR APP ID',
       },
-      "foursquare" : {
-        "secrets" : {
-          "redirectUrl" : "YOUR CALLBACK",
-          "clientId" : "YOUR CLIENT ID",
-          "clientSecret" : "YOUR CLIENT SECRET"
+      foursquare: {
+        secrets: {
+          redirectUrl: process.env.FSQ_REDIRECT,
+          clientId: process.env.FSQ_CLIENT_ID,
+          clientSecret: process.env.FSQ_CLIENT_SECRET,
         },
-        "startYear" : 2009
+        startYear: 2009,
       },
-      "defaults" : appDefaults
+      defaults: appDefaults,
     },
-    "defaultConfig" : "remote"
+    defaultConfig: 'remote',
   };
 
   function getConfig(n) {
-    if(!configs[n]) {
+    if (!configs[n]) {
       return configs[configs.defaultConfig];
     }
     return configs[n];
   }
 
   return {
-    "configs" : configs,
-    "getConfig" : getConfig
+    configs: configs,
+    getConfig: getConfig,
   };
-}();
+})();
--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-
 var sys = require('util'),
   fs = require('fs'),
   _ = require('underscore');
@@ -12,15 +11,22 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
     val = val || new Date().getMonth();
 
     var months = [
-      {'value' : 0, 'text' : '01'}, {'value' : 1, 'text' : '02'},
-      {'value' : 2, 'text' : '03'}, {'value' : 3, 'text' : '04'},
-      {'value' : 4, 'text' : '05'}, {'value' : 5, 'text' : '06'},
-      {'value' : 6, 'text' : '07'}, {'value' : 7, 'text' : '08'},
-      {'value' : 8, 'text' : '09'}, {'value' : 9, 'text' : '10'},
-      {'value' : 10, 'text' : '11'}, {'value' : 11, 'text' : '12'}];
+      { value: 0, text: '01' },
+      { value: 1, text: '02' },
+      { value: 2, text: '03' },
+      { value: 3, text: '04' },
+      { value: 4, text: '05' },
+      { value: 5, text: '06' },
+      { value: 6, text: '07' },
+      { value: 7, text: '08' },
+      { value: 8, text: '09' },
+      { value: 9, text: '10' },
+      { value: 10, text: '11' },
+      { value: 11, text: '12' },
+    ];
 
     _.each(months, function(n) {
-      if(n.value == val) {
+      if (n.value == val) {
         n.selected = true;
       }
     });
@@ -28,14 +34,15 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
   }
 
   function getYears(val) {
-    var years = [], cur = new Date().getFullYear();
+    var years = [],
+      cur = new Date().getFullYear();
     val = val || cur;
-    for(var i = cur; i >= config.foursquare.startYear; i--) {
+    for (var i = cur; i >= config.foursquare.startYear; i--) {
       var v = {
-        'value' : i,
-        'text' : i
+        value: i,
+        text: i,
       };
-      if(i == val) {
+      if (i == val) {
         v.selected = true;
       }
       years.push(v);
@@ -57,7 +64,7 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
 
   app.all('/', function getHome(req, res) {
     var data = {};
-    if(req.session.foursquare) {
+    if (req.session.foursquare) {
       data.session = req.session;
     }
     res.render('Home.jsx', data);
@@ -67,65 +74,68 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
     var r = req.query.r;
     logger.debug('REQUESTING: /login');
     var loc = Foursquare.getAuthClientRedirectUrl();
-    res.writeHead(303, { 'location': loc });
+    res.writeHead(303, { location: loc });
     res.end();
   });
 
-  app.get('/callback/foursquare', function getFoursquareCallback(req, res) {
+  app.get('/api/callback', function getFoursquareCallback(req, res) {
     logger.debug('REQUESTING: /callback/foursquare');
 
     logger.debug('ENTERING: processTokenCallback');
-    var code = req.query.code, redirect = req.query.r || '/trips';
+    var code = req.query.code,
+      redirect = req.query.r || '/trips';
 
-    Foursquare.getAccessToken({
-      code: code
-    }, function (error, access_token) {
-      if(error) {
-        logger.error('Error retrieving Access Token: ' + error.message);
-        res.redirect('/error?no_token');
-      }
-      else if(access_token) {
-        Foursquare.Users.getUser('self', access_token, function(error, user) {
-          if(error) {
-            res.redirect('/error?bad_user');
-          }
-          else {
-            req.session.foursquare = {
-              'user' : user.user,
-              'accessToken' : access_token
-            };
-            logger.debug('Redirecting to ' + redirect);
-            delete req.query.code;
-            delete req.query.c;
-            res.redirect(redirect);
-          }
-        });
-      }
-      else {
-        logger.error('No Access Token was received.');
-        res.redirect('/error?no_token');
-      }
-    });
+    Foursquare.getAccessToken(
+      {
+        code: code,
+      },
+      function(error, access_token) {
+        if (error) {
+          logger.error('Error retrieving Access Token: ' + error.message);
+          res.redirect('/error?no_token');
+        } else if (access_token) {
+          Foursquare.Users.getUser('self', access_token, function(error, user) {
+            if (error) {
+              res.redirect('/error?bad_user');
+            } else {
+              req.session.foursquare = {
+                user: user.user,
+                accessToken: access_token,
+              };
+              logger.debug('Redirecting to ' + redirect);
+              delete req.query.code;
+              delete req.query.c;
+              res.redirect(redirect);
+            }
+          });
+        } else {
+          logger.error('No Access Token was received.');
+          res.redirect('/error?no_token');
+        }
+      },
+    );
   });
 
   app.get('/disconnect', function(req, res) {
-    req.session.destroy(function(){
+    req.session.destroy(function() {
       res.redirect('/');
     });
   });
 
   function buildTripForm(request) {
-    var query = request.query, today = new Date(), form = {};
+    var query = request.query,
+      today = new Date(),
+      form = {};
     form.zip = query.zip || '';
-    if(query.a) {
+    if (query.a) {
       form.advanced = {
-        'startMonths' : getMonths(query.sm),
-        'startYears' : getYears(query.sy || today.getFullYear() - 1),
-        'endMonths' : getMonths(query.em),
-        'endYears' : getYears(query.ey || today.getFullYear()),
-        'homeRadius' : query.hr || config.defaults.homeRadius,
-        'cityRadius' : query.cr || config.defaults.cityRadius,
-        'tripMinimum' : query.tm || config.defaults.tripMinimum
+        startMonths: getMonths(query.sm),
+        startYears: getYears(query.sy || today.getFullYear() - 1),
+        endMonths: getMonths(query.em),
+        endYears: getYears(query.ey || today.getFullYear()),
+        homeRadius: query.hr || config.defaults.homeRadius,
+        cityRadius: query.cr || config.defaults.cityRadius,
+        tripMinimum: query.tm || config.defaults.tripMinimum,
       };
       form.id = JSON.stringify(form);
     }
@@ -140,9 +150,9 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
 
     //req.session.checkins = req.session.checkins || {};
 
-    if(form.zip) {
+    if (form.zip) {
       var selected = function(item) {
-        if(item.selected) {
+        if (item.selected) {
           logger.debug(sys.inspect(item));
         }
         return item.selected;
@@ -150,7 +160,7 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
 
       var options = {};
 
-      if(form.advanced) {
+      if (form.advanced) {
         var ey = parseInt(_.select(form.advanced.endYears, selected)[0].value),
           em = parseInt(_.select(form.advanced.endMonths, selected)[0].value),
           sy = parseInt(_.select(form.advanced.startYears, selected)[0].value),
@@ -159,26 +169,24 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
           after = new Date(sy, sm, 1, 0, 0, 0, 0);
 
         options = {
-          'after' : after,
-          'before' : before,
-          'homeRadius' : form.advanced.homeRadius,
-          'cityRadius' : form.advanced.cityRadius,
-          'tripMinimum' : form.advanced.tripMinimum
+          after: after,
+          before: before,
+          homeRadius: form.advanced.homeRadius,
+          cityRadius: form.advanced.cityRadius,
+          tripMinimum: form.advanced.tripMinimum,
         };
       }
 
       var process = function(error, trips, checkins, location) {
-
-        if(error) {
+        if (error) {
           logger.error(sys.inspect(error));
           res.redirect('/error');
-        }
-        else {
+        } else {
           //req.session.checkins[form.id] = checkins;
           var props = {
-            'data' : trips,
-            'user' : req.session.foursquare.user,
-            'form' : form
+            data: trips,
+            user: req.session.foursquare.user,
+            form: form,
           };
           res.render('Trips.jsx', props);
         }
@@ -191,14 +199,13 @@ exports.addRoutes = module.exports.addRoutes = function(app, config) {
         Backstroke.buildTripsByZip(req.session.checkins[form.id], form.zip, options, process);
       }
       else {*/
-        logger.debug('Retrieving checkins to build trips.');
-        Backstroke.retrieveTrips(form.zip, options, accessToken, process);
+      logger.debug('Retrieving checkins to build trips.');
+      Backstroke.retrieveTrips(form.zip, options, accessToken, process);
       /*}*/
-    }
-    else {
+    } else {
       var props = {
-        'data' : null,
-        'form' : form
+        data: null,
+        form: form,
       };
       res.render('Trips.jsx', props);
     }
